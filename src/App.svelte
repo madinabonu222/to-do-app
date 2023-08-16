5527e6af4134e10643f8e3b87977228ebd38e437
madinabonu <misssonmi25@gmail.com>
2023-08-16 21:27:40 +0900

    web storage

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { onMount } from "svelte";
   import "./index.css";
   export let name;
 
@@ -7,6 +8,14 @@
     // { done: false, text: "prepare for driving test" },
     // { done: false, text: "washing dishes" },
   ];
+  let searchText = "";
+  // Load todos from localStorage on component mount
+  onMount(() => {
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      todos = JSON.parse(storedTodos);
+    }
+  });
 
   function add() {
     todos = todos.concat({ done: false, text: "" });
@@ -15,17 +24,29 @@
   function clear() {
     todos = todos.filter((t) => !t.done);
   }
+  function filter(todo) {
+    todos = todos.filter((t) => t !== todo);
+  }
+  function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
   $: remaining = todos.filter((t) => !t.done).length;
+
+  // Save todos to localStorage whenever todos or searchText changes
+  $: {
+    saveTodos();
+  }
 </script>
 
 <header>
-  <div class="box1">
+  <div class="header">
     <h1>{name}!</h1>
     <h2>What do you need to do?</h2>
   </div>
 </header>
 
-<main class="box2">
+<main class="main">
   {#each todos as todo}
     <div class:done={todo.done}>
       <input type="checkbox" bind:checked={todo.done} />
@@ -33,6 +54,16 @@
       <input placeholder="What needs to be done?" bind:value={todo.text} />
     </div>
   {/each}
+  <div id="search-input-cont">
+    <input
+      type="text"
+      id="search-field"
+      placeholder="Filter todos"
+      autocapitalize="off"
+      bind:value={todos}
+      on:input
+    />
+  </div>
   <p>{remaining} remaining</p>
 
   <button on:click={add}> Add new </button>
@@ -41,17 +72,34 @@
 </main>
 
 <style>
-  .box1 {
+  .header {
     background-color: rgb(102, 0, 255);
     opacity: 0.8;
     height: 100%;
     width: 100%;
     display: inline-block;
   }
+  #search-input-cont {
+    width: 14%;
+    display: flex;
+    align-items: center;
+    font-family: "Comic Sans MS", cursive;
+    margin: 0 0 0 1px;
+  }
 
-  .box2 {
+  #search-field {
+    width: 100%;
+    font-size: 1.1rem;
+    border: 3px solid rgba(48, 17, 187, 0.635);
+    border-radius: 5px;
+    padding: 5px;
+    margin: 0 0px 0;
+  }
+
+  .main {
     background-color: darkgrey;
     opacity: 10;
+    /* display: block; */
   }
 
   header {
